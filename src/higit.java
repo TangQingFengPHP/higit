487061ef6a9d13d36018c33941c509ed58f84505
panfeng <2393637503@qq.com>
2018-04-24 09:39:53 +0800

    test1

---
+++ src/higit.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: higit
+ * Author:   Administrator
+ * Date:     2018/4/24 9:33
+ * Description: higit
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈higit〉
+ *
+ * @author Administrator
+ * @create 2018/4/24
+ * @since 1.0.0
+ */
+public class higit {
+
+    public static void main(String[]args){
+
+        System.out.print("<h1>higit</h1>");
+    }
+
+}
